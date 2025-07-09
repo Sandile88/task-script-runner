dace0703a4a546660b3212b0a8b2cfbeb7c8e62e
Sandile88 <sandilep.mremi@gmail.com>
2025-07-09 15:55:47 +0200

    added log method

--- src/main/java/task/exec/database/ExecutionLogger.java
+++ src/main/java/task/exec/database/ExecutionLogger.java
@@ -1,12 +1,14 @@
 package task.exec.database;
 
 import java.sql.*;
+import java.time.LocalDateTime;
 
 public class ExecutionLogger {
     private final String dbUrl = "jdbc:sqlite:logs.db";
 
     public ExecutionLogger() {
-        try (Connection conn = DriverManager.getConnection(dbUrl);
+        try (
+            Connection conn = DriverManager.getConnection(dbUrl);
             Statement stmt = conn.createStatement()) {
             String sql = """
              CREATE TABLE IF NOT EXISTS scripts_run (
@@ -20,5 +22,22 @@ public class ExecutionLogger {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public void log(String scriptName, int exitCode) {
+        String sql = "INSERT INTO scripts_run (script_name, timestamp, exit_code) VALUES (?, ?, ?)";
+        try {
+            Connection conn = DriverManager.getConnection(dbUrl); 
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                pstmt.setString(1, scriptName);
+                pstmt.setString(2, LocalDateTime.now().toString());
+                pstmt.setInt(3, exitCode);
+                pstmt.executeUpdate();
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 }
